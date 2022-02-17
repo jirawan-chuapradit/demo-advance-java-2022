a2316ff81796373db5936f1dd4850836d514326e
jirawan.c <jirawan.c@kbtg.tech>
2022-02-17 13:27:44 +0700

    first name is null

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -9,14 +9,23 @@ class RegisterBusinessTest {
 
     @Test
     @DisplayName("ข้อมูล speaker = null จะโยน runtime exception  กลับมา "+"พร้อมกับ message speaker is null")
-    public void register() {
+    public void case01() {
         RegisterBusiness business = new RegisterBusiness();
 
         // assert exception with JUnit 5
         Exception exception= assertThrows(RuntimeException.class, ()-> {
             business.register(null,null);
         });
-
         assertEquals("speaker is null", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("First name = null ดังนั้นจะ โยน argumentNullexception ออกมา " + "พร้อมกับคำว่า first name is required")
+    public void case02() {
+        RegisterBusiness business = new RegisterBusiness();
+        Exception exception= assertThrows(ArgumentNullException.class, ()-> {
+            business.register(null,new Speaker());
+        });
+       assertEquals("First name is required.",exception.getMessage());
     }
 }
