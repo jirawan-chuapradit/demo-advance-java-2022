2f493aaa4e1eff96efa88c30470ee6dff3c30f5e
jirawan.c <jirawan.c@kbtg.tech>
2022-02-17 11:05:41 +0700

    Intro Test Struct

--- src/test/java/demo/HelloWorldTest.java
+++ src/test/java/demo/HelloWorldTest.java
@@ -3,12 +3,22 @@ package demo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class HelloWorldTest {
 
     @Test
-    @DisplayName("เรากำลังทดสอบเรื่องอะไร อย่างไร ต้องได้อะไร")
+    @DisplayName("เรากำลังทดสอบ Hello function ส่งJJ เข้าไป ต้องได้ Hello JJ")
     public void case01() {
+        // Arrange, Given
+        HelloWorld helloWorld =  new HelloWorld();
+        // Act, When
+        String actualResult = helloWorld.hi("JJ");
+        // Assert/ verify, Then
+        assertEquals("Hello JJ",actualResult);
     }
+
+
 
     @Test
     public void case02() {
