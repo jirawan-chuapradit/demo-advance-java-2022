889c676e06301bdb6678d181104a683a176cd5f7
jirawan.c <jirawan.c@kbtg.tech>
2022-02-18 11:43:09 +0700

    add interface implements

--- src/main/java/register/DemoService.java
+++ src/main/java/register/DemoService.java
@@ -13,9 +13,6 @@ public class DemoService {
 
 }
 
-class DB {
-    int increaseOne(int id) {
-        // return id + 1
-        throw new RuntimeException("Under construction");
-    }
+interface DB {
+    int increaseOne(int id) ;
 }
--- src/test/java/register/DemoServiceTest.java
+++ src/test/java/register/DemoServiceTest.java
@@ -8,14 +8,16 @@ class DemoServiceTest {
 
     @Test
     void case_1_should_get_2() {
-        DB db = new DB() {
-            @Override
-            int increaseOne(int id) {
-                return 2;
-            }
-        };
+        DB db = new DBx();
         DemoService demoService = new DemoService(db);
         String actualResult = demoService.compute(1);
         assertEquals("Result = 2", actualResult);
+    }
+}
+
+class  DBx implements  DB {
+    @Override
+    public int increaseOne(int id) {
+        return 2;
     }
 }
