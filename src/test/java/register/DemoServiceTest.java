927c2228487390cc4721ea75bfe6e7b2bc06c14f
jirawan.c <jirawan.c@kbtg.tech>
2022-02-18 12:14:37 +0700

    add spy test

--- src/test/java/register/DemoServiceTest.java
+++ src/test/java/register/DemoServiceTest.java
@@ -13,11 +13,33 @@ class DemoServiceTest {
         String actualResult = demoService.compute(1);
         assertEquals("Result = 2", actualResult);
     }
+
+    @Test
+    void case_2_test_spy() {
+        SpyDBx spyDBx = new SpyDBx();
+        DemoService demoService = new DemoService(spyDBx);
+       demoService.compute(1);
+        spyDBx.verify(1);
+    }
 }
 
 class  DBx implements  DB {
     @Override
     public int increaseOne(int id) {
+        return 2;
+    }
+}
+
+class  SpyDBx implements  DB {
+   private  int count;
+
+   public  void  verify(int count) {
+    assertTrue(count == this.count, "must call count = "+ count + "but"+ "call = "+ this.count);
+   }
+
+    @Override
+    public int increaseOne(int id) {
+        count++;
         return 2;
     }
 }
